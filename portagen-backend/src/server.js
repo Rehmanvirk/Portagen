0547ef887af2ba103f26c84e153e87ade3c224ae
Abdul Rehman <rehmanvirk318@gmail.com>
2025-11-09 23:08:22 +0500

    added cors

--- portagen-backend/src/server.js
+++ portagen-backend/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 // app.use(cors());
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://portagen.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
